e4db22d041cefa7480ed5d654b49c1c51105e39f
d1vent <dodinhtruong2003xxz@gmail.com>
2023-09-25 12:12:11 +0700

    new repos

---
+++ src/list/SingleLinkedList.java
@@ -0,0 +1,58 @@
+package list;
+
+public class SingleLinkedList {
+    Node head = null;
+    public class Node
+    {
+        String data;
+        Node next;
+
+        public Node(String data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    public void append(String data)
+    {
+        head = append(data,head);
+    }
+    private Node append(String data, Node node)
+    {
+        if(node == null) return new Node(data,null);
+        node.next = append(data, node.next);
+        return node;
+    }
+    public void removeFirst(String data)
+    {
+        head = removeFirst(data, head);
+    }
+    private Node removeFirst(String data, Node node)
+    {
+        if(node == null) return null;
+        if(node.data.equals(data)) return node.next; // neu node hien tai chua data -> xoa luon(tra ve node ke tiep)
+        node.next = removeFirst(data, node.next);
+        return node;
+    }
+
+    public void removeAll()
+    public String toString()
+    {
+        return "[" + toString(head) + "]";
+    }
+    private String toString(Node node)
+    {
+        if(node == null) return "";
+        return node.data + toString(node.next);
+    }
+    public static void main(String[] args)
+    {
+        SingleLinkedList list = new SingleLinkedList();
+        list.append("One");
+        list.append("Two");
+        list.append("Three");
+        list.append("One");
+        System.out.println(list);
+
+    }
+}
