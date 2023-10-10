46021e37fa94214b3c3ea40d67895b0cea029e19
d1vent <dodinhtruong2003xxz@gmail.com>
2023-10-10 22:57:25 +0700

    w6 update

---
+++ src/Sort/SeletionSort1.java
@@ -0,0 +1,150 @@
+package Sort;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Comparator;
+
+/**
+ *  The {@code Selection} class provides static methods for sorting an
+ *  array using <em>selection sort</em>.
+ *  This implementation makes ~ &frac12; <em>n</em><sup>2</sup> compares to sort
+ *  any array of length <em>n</em>, so it is not suitable for sorting large arrays.
+ *  It performs exactly <em>n</em> exchanges.
+ *  <p>
+ *  This sorting algorithm is not stable. It uses &Theta;(1) extra memory
+ *  (not including the input array).
+ *  <p>
+ *  For additional documentation, see
+ *  <a href="https://algs4.cs.princeton.edu/21elementary">Section 2.1</a>
+ *  of <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class SeletionSort1 {
+
+    private static Comparator comparator;
+
+    // This class should not be instantiated.
+    private SeletionSort1() { }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(int[] a) {
+        int n = a.length;
+        int i;
+        for (i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (less(a[j], a[min])) min = j;
+            }
+            exch(a, i, min);
+            assert isSorted(a, comparator, 0, i);
+        }
+        assert isSorted(a, comparator, 0, i);
+    }
+
+
+
+    private static boolean isSorted(int[] a, Comparator comparator, int lo, int i) {
+        return false;
+    }
+
+    /**
+     * Rearranges the array in ascending order, using a comparator.
+     * @param a the array
+     * @param comparator the comparator specifying the order
+     */
+    public static void sort(int[] a, Comparator comparator) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i+1; j < n; j++) {
+                if (less(comparator, a[j], a[min])) min = j;
+            }
+            exch(a, i, min);
+            assert isSorted(a, comparator, 0, i);
+        }
+        assert isSorted(a, comparator);
+    }
+
+
+    /***************************************************************************
+     *  Helper sorting functions.
+     ***************************************************************************/
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    // is v < w ?
+    private static boolean less(Comparator comparator, Object v, Object w) {
+        return comparator.compare(v, w) < 0;
+    }
+
+
+    // exchange a[i] and a[j]
+    private static void exch(int[] a, int i, int j) {
+        int swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+    /***************************************************************************
+     *  Check if array is sorted - useful for debugging.
+     ***************************************************************************/
+
+    // is the array a[] sorted?
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    // is the array sorted from a[lo] to a[hi]
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    // is the array a[] sorted?
+    private static boolean isSorted(int[] a, Comparator comparator) {
+        return isSorted(a, comparator, 0, a.length - 1);
+    }
+
+    // is the array sorted from a[lo] to a[hi]
+    private static boolean isSorted(Object[] a, Comparator comparator, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(comparator, a[i], a[i-1])) return false;
+        return true;
+    }
+
+
+
+    // print array to standard output
+    private static void show(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+    /**
+     * Reads in a sequence of strings from standard input; selection sorts them;
+     * and prints them to standard output in ascending order.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        In in = new In("C:\\Users\\TRUONG\\IdeaProjects\\algs4-data\\4Kints.txt");
+        int[] a = in.readAllInts();
+        long start = System.currentTimeMillis();
+        SeletionSort1.sort(a);
+        long end = System.currentTimeMillis();
+        show(a);
+        System.out.println("Elasped time: " + (end - start));
+    }
+}
